92cca739ad3b6123e07d6db496ea3d2540537a09
skeptrune <110786511+skeptrune@users.noreply.github.com>
2023-06-25 04:34:49 +0000

    bugfix: remove default plausible provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,8 @@
 import '../styles/globals.css'
-import PlausibleProvider from 'next-plausible'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <PlausibleProvider domain="aftertax.io">
       <Component {...pageProps} />
-    </PlausibleProvider>
   )
 }
 
